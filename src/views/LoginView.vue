705e557035694f96cb4846b0af7542dab78009d0
vladpostu <vladpostu@gmail.com>
2022-06-21 16:17:13 +0200

    login and register function

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,11 +7,44 @@
         <input type="text" name="email" placeholder="email@adress.com" />
       </div>
       <div class="input">
-        <label for="email">Password</label>
-        <input type="password" name="email" placeholder="password123" />
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="password123" />
       </div>
 
       <button type="submit" id="login_button">Login</button>
     </form>
   </div>
 </template>
+
+<script>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login(submitEvent) {
+      this.email = submitEvent.target.elements.email.value;
+      this.password = submitEvent.target.elements.password.value;
+
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          this.$router.push("/dashboard");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+        });
+    },
+  },
+};
+</script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -7,11 +7,47 @@
         <input type="text" name="email" placeholder="email@adress.com" />
       </div>
       <div class="input">
-        <label for="email">Password</label>
-        <input type="password" name="email" placeholder="password123" />
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="password123" />
       </div>
 
-      <button type="submit" id="login_button">Login</button>
+      <button type="submit" id="register_button">Register</button>
     </form>
   </div>
 </template>
+
+<script>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "123456",
+    };
+  },
+  methods: {
+    register(submitEvent) {
+      // data update
+      this.email = submitEvent.target.elements.email.value;
+      this.password = submitEvent.target.elements.password.value;
+
+      // firebase registration
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          console.log("Registration completed");
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+        });
+    },
+  },
+};
+</script>
